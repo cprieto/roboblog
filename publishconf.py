8a96df5a0e4df0a17f37390dcbbb8d2b13b95748
Cristian Prieto <me@cprieto.com>
2019-02-08 12:29:48 +0100

    Updated to latest pelican version
     - A few changes were needed to update
     - Using now Invoke instead of Fab
     - Updated font to use ligatures, now we use Iosevka

--- publishconf.py
+++ publishconf.py
@@ -4,11 +4,9 @@ from __future__ import unicode_literals
 
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
-
 import os
 import sys
 sys.path.append(os.curdir)
-
 from pelicanconf import *
 
 SITEURL = 'http://cprieto.com'
@@ -16,8 +14,6 @@ RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'atom.xml'
 CATEGORY_FEED_ATOM = ''
-
-DELETE_OUTPUT_DIRECTORY = False
 
 # Following items are often useful when publishing
 
